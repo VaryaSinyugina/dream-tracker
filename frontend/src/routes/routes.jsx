bd0ed29606c779d0388613a32753673ec5a613b6
LenaVishnyakova <vishnyakova1213@gmail.com>
2025-09-11 19:17:19 +0900

    новая структура на vite

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,13 @@
+// import { Route, Routes } from "react-router-dom"
+
+export default function AppRoutes() {
+    return (
+        // <Routes>
+        //     <Route path="/">
+                
+        //     </Route>
+        // </Routes>
+        <>
+        </>
+    )
+}
